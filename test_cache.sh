62423e58a4e62baae158f2221092872d3d980f2f
John Bumgardner <john.bumgardner@icloud.com>
2021-07-29 11:52:23 -0400

    modify test cache

--- test_cache.sh
+++ test_cache.sh
@@ -6,20 +6,18 @@ for protocol in 0 1
 do
  for cachesize in 32000 64000 128000 256000 512000
  do
-  echo "./dsm $cachesize 4 64 16 $protocol $swaptions_truncated" > real.$OUTN
-  ./dsm $cachesize 4 64 16 $protocol $swaptions_truncated >> real.$OUTN
+  
   echo "./simulate_cache_ref $cachesize 4 64 16 $protocol $swaptions_truncated" > ref.$OUTN
   ./simulate_cache_ref $cachesize 4 64 16 $protocol $swaptions_truncated >> ref.$OUTN
-  diff real.$OUTN ref.$OUTN > diff.$OUTN
+  
   OUTN=$(($OUTN+1))
  done
  for blocksize in 64 128 256
  do
-  echo "./dsm 1000000 4 $blocksize 16 $protocol $swaptions_truncated" > real.$OUTN
-  ./dsm 1000000 4 $blocksize 16 $protocol $swaptions_truncated >> real.$OUTN
+  
   echo "./simulate_cache_ref 1000000 4 $blocksize 16 $protocol $swaptions_truncated" > ref.$OUTN
   ./simulate_cache_ref 1000000 4 $blocksize 16 $protocol $swaptions_truncated >> ref.$OUTN
-  diff real.$OUTN ref.$OUTN > diff.$OUTN
+  
   OUTN=$(($OUTN+1))
  done
 done
